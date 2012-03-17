a525d25b9b79539bd7f7f93dacaba2f1b26126e1
Niel de la Rouviere <nieldlr@gmail.com>
2012-03-17 14:16:57 +0200

    First commit

---
+++ hanzidecomposer.js
@@ -0,0 +1,42 @@
+var fs = require('fs');
+var characters = new Array();
+
+function start(){
+	var i=0;
+	var lines = new Array();
+	
+	console.log("Reading in File... Be patient. It may take a while");
+	var readFile = fs.readFileSync('./charData.txt', 'utf-8');
+	lines = readFile.split(/\r?\n/);
+	//console.log(lines.length);
+		
+	for(i=0;i<lines.length; i++){
+		var colonsplit = lines[i].split(':');
+		var character = colonsplit[0];
+		var decomposition = colonsplit[1];
+		var openbracket = decomposition.indexOf('(');
+		var closebracket = decomposition.indexOf(')');
+		var typeOfDecomposition = decomposition.substring(0, openbracket);
+		var components = decomposition.substring(openbracket+1, closebracket).split(",");
+		characters[i] = new Array();
+		characters[i][0] = character;
+		characters[i][1] = typeOfDecomposition;
+		characters[i][2] = components;
+		/*console.log(character);
+		console.log(typeOfDecomposition);
+		console.log(components);*/
+	};
+	
+	console.log('Files Read');
+};
+
+var decompose = function(character){
+	for(var i = 0; i<characters.length; i++){
+		if(characters[i][0] == character){
+			console.log(characters[i][2]);
+		}
+	}
+};
+
+exports.start = start;
+exports.decompose = decompose;
