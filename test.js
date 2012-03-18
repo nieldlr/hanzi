c0732d754b191cb3d2194ba9eebba8b81d5d64a5
Niel de la Rouviere <nieldlr@gmail.com>
2012-03-18 12:41:20 +0200

    More functions

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
-var hanzi = require("./hanzidecomposer.js");
+var hanzi = require('./lib/module.js');
 hanzi.start();
-var radicals = hanzi.decompose('河');
+var radicals = hanzi.decompose('微');
 console.log(radicals);
 var radicals2 = hanzi.decompose('水');
 console.log(radicals2);
+var definition = hanzi.dictionaryLookup('微', 's');
+console.log(definition);
