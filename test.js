7c780d0fb5bb6591b55759033296a0afaf49420f
Niel de la Rouviere <nieldlr@gmail.com>
2012-03-18 10:28:46 +0200

    Added more testing functions

--- test.js
+++ test.js
@@ -2,3 +2,5 @@ var hanzi = require("./hanzidecomposer.js");
 hanzi.start();
 var radicals = hanzi.decompose('河');
 console.log(radicals);
+var radicals2 = hanzi.decompose('水');
+console.log(radicals2);
