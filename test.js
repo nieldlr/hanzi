b7a06c878ed8eda303a1034590cf3173dc1b4e98
Niel de la Rouviere <nieldlr@gmail.com>
2012-03-19 12:36:12 +0200

    Added test function to test reduced components

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var hanzi = require('./lib/module.js');
 hanzi.start();
 console.log(hanzi.decompose('微'));
-console.log(hanzi.decompose('水'));
+console.log(hanzi.decompose('氵'));
 console.log(hanzi.definitionLookup('微', 's'));
