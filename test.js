fad9024c1f5f384c55d3cbbb2ec20b9345c2cc72
Niel de la Rouviere <nieldlr@gmail.com>
2012-09-30 09:59:29 +0200

    Just additional function to test HanziJS

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
 var hanzi = require('./lib/module.js');
 hanzi.start();
-var decomposition = hanzi.decompose('河', 1);
+var decomposition = hanzi.decompose('搬', 1);
 console.log("Test radical decomposition of simplifed character: " + decomposition.character + " => " + decomposition.components);
-var decomposition = hanzi.decompose('河', 2);
+var decomposition = hanzi.decompose('搬', 2);
 console.log("Test graphical decomposition of simplifed character: " + decomposition.character + " => " + decomposition.components);
+var decomposition = hanzi.decompose('搬', 3);
+console.log("Test once decomposition of simplifed character: " + decomposition.character + " => " + decomposition.components);
 
 var decomposition = hanzi.decompose('登', 1);
 console.log("Test radical decomposition of simplifed character: " + decomposition.character + " => " + decomposition.components);
-var decomposition = hanzi.decompose('登', 2);
+var decomposition = hanzi.decompose('雜', 1);
 console.log("Test graphical decomposition of simplifed character: " + decomposition.character + " => " + decomposition.components);
 console.log("---------");
-/*var decomposition = hanzi.decompose('亂');
-console.log("Test decomposition of complex traditional character: " + decomposition.character + " => " + decomposition.components);
-//var decomposition = hanzi.decompose('雜');
-console.log("Test decomposition of complex traditional character: " + decomposition.character + " => " + decomposition.components);
 
-console.log(hanzi.definitionLookup('和', 's'));
-console.log(hanzi.getPinyin('我', 's'));*/
+console.log(hanzi.definitionLookup('和'));
+console.log(hanzi.definitionLookup('和乐'));
+//console.log(hanzi.getPinyin('我', 's'));
+//console.log(hanzi.comparePhoneticRadical('挂', '圭'));
