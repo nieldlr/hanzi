3ac96708f97a88c657c3876dbac33bc0f6aaa13f
Niel de la Rouviere <nieldlr@gmail.com>
2012-11-04 15:39:49 +0200

    Added a function to getExample words from corpus data compiled by Leiden University on one month of Weibo posts

--- test.js
+++ test.js
@@ -12,8 +12,12 @@ console.log(hanzi.comparePhoneticRadical('搬', decomposition.components1[1]));
 var decompositionmany = hanzi.decomposeMany("和挂搬");
 console.log(decompositionmany);
 
-var search = hanzi.dictionarySearch('光');
-console.log(search);
+//var search = hanzi.dictionarySearch('光');
+//console.log(search);
+
+var examples = hanzi.getExamples("挂");
+//console.log(examples);
+
 //console.log(hanzi.definitionLookup('和'));
 //console.log(hanzi.definitionLookup('和乐'));
 //console.log(hanzi.getPinyin('我', 's'));
