2c7b2e08630ff2e6cd588f0185de007ef9e4eba4
Niel de la Rouviere <nieldlr@gmail.com>
2012-11-15 10:27:06 +0200

    Standardize test file

--- test.js
+++ test.js
@@ -1,25 +1,46 @@
 var hanzi = require('./lib/module.js');
 hanzi.start();
-var decomposition = hanzi.decompose('搬');
+
+//Simplified Tests
+
+console.log("SIMPLIFIED CHARACTER TESTS");
+
+var decomposition = hanzi.decompose('爱');
 console.log("Test once decomposition of simplifed character: " + decomposition.character + " => " + decomposition.components1);
-//var decomposition = hanzi.decompose('搬', 2);
 console.log("Test radical decomposition of simplifed character: " + decomposition.character + " => " + decomposition.components2);
-//var decomposition = hanzi.decompose('搬', 3);
 console.log("Test graphical decomposition of simplifed character: " + decomposition.character + " => " + decomposition.components3);
 
 console.log("---------");
-console.log(hanzi.comparePhoneticRadical('搬', decomposition.components1[1]));
-var decompositionmany = hanzi.decomposeMany("和挂搬");
+
+var decompositionmany = hanzi.decomposeMany("和挂爱");
+console.log("Test many decomposition: ");
 console.log(decompositionmany);
 
-//var search = hanzi.dictionarySearch('我是见很吗不好谢在你谁什她么的也这他那们对去家怎做起哪想里在先太姐请名问生小叫字', 'only');
-//console.log(search);
-//console.log(search.length);
+var singlelookup = hanzi.definitionLookup('爱');
+console.log("Test single lookup of dictionary entry: ");
+console.log(singlelookup);
 
-var examples = hanzi.getExamples("挂");
+var examples = hanzi.getExamples("爱");
+console.log("Test examples words from character: ");
 console.log(examples);
+console.log(examples[0]);
 
-//console.log(hanzi.definitionLookup('和'));
-//console.log(hanzi.definitionLookup('和乐'));
-//console.log(hanzi.getPinyin('我', 's'));
-//console.log(hanzi.comparePhoneticRadical('挂', '圭'));
+//Traditional Tests
+
+console.log("TRADITIONAL CHARACTER TESTS");
+
+var decomposition = hanzi.decompose('愛');
+console.log("Test once decomposition of simplifed character: " + decomposition.character + " => " + decomposition.components1);
+console.log("Test radical decomposition of simplifed character: " + decomposition.character + " => " + decomposition.components2);
+console.log("Test graphical decomposition of simplifed character: " + decomposition.character + " => " + decomposition.components3);
+
+console.log("---------");
+
+var singlelookup = hanzi.definitionLookup('愛');
+console.log("Test single lookup of dictionary entry: ");
+console.log(singlelookup);
+
+var examples = hanzi.getExamples("愛");
+console.log("Test examples words from character: ");
+console.log(examples);
+console.log(examples[0]);
