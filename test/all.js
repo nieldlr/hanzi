1daef999580a43d03bb9298304d1e6b08e8a0483
Niel de la Rouviere <nieldlr@gmail.com>
2013-01-07 09:46:59 +0800

    Better number handling merged and bumped to v0.2.7

--- test/all.js
+++ test/all.js
@@ -12,7 +12,7 @@ describe('hanzidecomposer', function(){
 	});
 
 	it('should once decompose simplified character', function(){
-		assert.deepEqual(hanzi.decompose('爱').components1, ['37333', '友']);
+		assert.deepEqual(hanzi.decompose('爱').components1, ['No glyph available', '友']);
 	});
 	it('should radical decompose simplified character', function(){
 		assert.deepEqual(hanzi.decompose('爱').components2, ['爫', '冖', '𠂇', '又']);
@@ -22,7 +22,7 @@ describe('hanzidecomposer', function(){
 	});
 
 	it('should once decompose traditional character', function(){
-		assert.deepEqual(hanzi.decompose('愛').components1, ['38462','夂']);
+		assert.deepEqual(hanzi.decompose('愛').components1, ['No glyph available','夂']);
 	});
 	it('should radical decompose traditional character', function(){
 		assert.deepEqual(hanzi.decompose('愛').components2, ['爫','冖','心','夂']);
