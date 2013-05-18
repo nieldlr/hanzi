9e143a139088d8685442a9e89c5d7107ff7f52f0
Dušan Juretić <dusan.juretic@gmail.com>
2013-05-18 16:22:49 -0400

    Avoid adding methods to String prototype

--- test/all.js
+++ test/all.js
@@ -22,6 +22,18 @@ describe('hanzidecomposer', function(){
 		assert(hanzi.getRadicalMeaning('氵'), "water");
 	});
 
+	it("determines phonetic regularity", function(){
+		var expected = {
+			di1: {
+				character: '低',
+				component: [ '亻', '氐', '氐', '亻', '氏', '氏', '丶', '丶' ],
+				phoneticpinyin: [ 'ren2', 'di1', 'di3', 'ren2', 'shi4', 'zhi1', 'dian3', 'zhu3' ],
+				regularity: [ 0, 1, 2, 0, 4, 4, 3, 0 ]
+			}
+		};
+		assert.deepEqual(hanzi.determinePhoneticRegularity('低'), expected);
+	});
+
 	it('should once decompose simplified character', function(){
 		assert.deepEqual(hanzi.decompose('爱').components1, ['No glyph available', '友']);
 	});
